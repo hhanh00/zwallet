012f39e0001fb6f4bba0ad5daa0ea50319fc71ef
Hanh <hanh425@gmail.com>
2021-07-23 16:12:54 +0800

    Use github

--- native/zcash_params/README.md
+++
@@ -1,2 +0,0 @@
-This package just bundles the Zcash sapling circuit parameters
-as a crate. To build, copy the `*.params` files into `src`.
--- native/zcash_params/src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub const SPEND_PARAMS: &'static [u8] = include_bytes!("sapling-spend.params");
-pub const OUTPUT_PARAMS: &'static [u8] = include_bytes!("sapling-output.params");
-pub const GENERATORS: &'static [u8] = include_bytes!("generators.bin");
