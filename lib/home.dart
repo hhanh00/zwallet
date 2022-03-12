f0945d82b54f62e32d79f1b1a3496874a557e678
Hanh <hanh425@gmail.com>
2022-03-12 21:09:08 +0800

    Cleaner simple mode tab switch

--- lib/home.dart
+++ lib/home.dart
@@ -17,28 +17,13 @@ import 'main.dart';
 import 'note.dart';
 import 'store.dart';
 
-class HomePage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Observer(builder: (context) {
-      final simpleMode = settings.simpleMode;
-      return HomePageInner(simpleMode);
-    });
-  }
-}
-
-class HomePageInner extends StatefulWidget {
-  final bool simpleMode;
-  HomePageInner(this.simpleMode);
+class HomePage extends StatefulWidget {
   @override
   HomeState createState() => HomeState();
 }
 
-class HomeState extends State<HomePageInner> with TickerProviderStateMixin {
-  TabController? _tabController;
-  int _tabIndex = 0;
+class HomeState extends State<HomePage> {
   StreamSubscription? _syncDispose;
-  final contactKey = GlobalKey<ContactsState>();
 
   @override
   void initState() {
@@ -72,19 +57,6 @@ class HomeState extends State<HomePageInner> with TickerProviderStateMixin {
   }
 
   @override
-  void didChangeDependencies() {
-    _tabController?.dispose();
-    final tabController = TabController(length: settings.simpleMode ? 3 : 6, vsync: this);
-    tabController.addListener(() {
-      setState(() {
-        _tabIndex = tabController.index;
-      });
-    });
-    _tabController = tabController;
-    super.didChangeDependencies();
-  }
-
-  @override
   void dispose() {
     _syncDispose?.cancel();
     super.dispose();
@@ -92,81 +64,113 @@ class HomeState extends State<HomePageInner> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) => Observer(builder: (context) {
-        final s = S.of(context);
-        final theme = Theme.of(context);
-        final simpleMode = settings.simpleMode;
-
-        if (active.id == 0) {
-          return AccountManagerPage();
-        }
-
-        final contactTabIndex = simpleMode ? 2 : 5;
-        Widget button = Container();
-        if (_tabIndex == 0)
-            button = FloatingActionButton(
-              onPressed: _onSend,
-              backgroundColor: theme.colorScheme.secondary,
-              child: Icon(Icons.send),
-            );
-        else if (_tabIndex == contactTabIndex)
-            button = FloatingActionButton(
-            onPressed: _onAddContact,
-            backgroundColor: theme.colorScheme.secondary,
-            child: Icon(Icons.add),
-            );
-
-        final menu = PopupMenuButton<String>(
-          itemBuilder: (context) {
-            return [
-              PopupMenuItem(child: Text(s.accounts), value: "Accounts"),
-              PopupMenuItem(child: Text(s.backup), value: "Backup"),
-              PopupMenuItem(child: Text(s.rescan), value: "Rescan"),
-              if (!simpleMode && active.canPay)
-                PopupMenuItem(child: Text(s.coldStorage), value: "Cold"),
-              if (!simpleMode)
-                PopupMenuItem(child: Text(s.multipay), value: "MultiPay"),
-              if (!simpleMode)
-                PopupMenuItem(child: Text(s.broadcast), value: "Broadcast"),
-              PopupMenuItem(child: Text(s.settings), value: "Settings"),
-              PopupMenuItem(child: Text(s.help), value: "Help"),
-              PopupMenuItem(child: Text(s.about), value: "About"),
-            ];
-          },
-          onSelected: _onMenu,
-        );
-
-        return Scaffold(
-          appBar: AppBar(
-            centerTitle: true,
-            title: Text("${active.account.name}"),
-            bottom: TabBar(
-              controller: _tabController,
-              isScrollable: true,
-              tabs: [
-                Tab(text: s.account),
-                if (!simpleMode) Tab(text: s.notes),
-                Tab(text: s.history),
-                if (!simpleMode) Tab(text: s.budget),
-                if (!simpleMode) Tab(text: s.tradingPl),
-                Tab(text: s.contacts),
-              ],
-            ),
-            actions: [menu],
-          ),
-          body: TabBarView(
-            controller: _tabController,
-            children: [
-              AccountPage2(),
-              if (!simpleMode) NoteWidget(),
-              HistoryWidget(),
-              if (!simpleMode) BudgetWidget(),
-              if (!simpleMode) PnLWidget(),
-              ContactsTab(key: contactKey),
-            ],
-          ),
-          floatingActionButton: button,
-        );
+        final _simpleMode = settings.simpleMode;
+        final key = UniqueKey();
+        return HomeInnerPage(key: key);
       });
+}
+
+class HomeInnerPage extends StatefulWidget {
+  HomeInnerPage({Key? key}) : super(key: key);
+
+  @override
+  HomeInnerState createState() => HomeInnerState();
+}
+
+class HomeInnerState extends State<HomeInnerPage> with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+  int _tabIndex = 0;
+  final contactKey = GlobalKey<ContactsState>();
+
+  @override
+  void initState() {
+    super.initState();
+    final tabController = TabController(length: settings.simpleMode ? 3 : 6, vsync: this);
+    tabController.addListener(() {
+      setState(() {
+        _tabIndex = tabController.index;
+      });
+    });
+    _tabController = tabController;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final s = S.of(context);
+    final theme = Theme.of(context);
+    final simpleMode = settings.simpleMode;
+
+    if (active.id == 0) {
+      return AccountManagerPage();
+    }
+
+    final contactTabIndex = simpleMode ? 2 : 5;
+    Widget button = Container();
+    if (_tabIndex == 0)
+      button = FloatingActionButton(
+        onPressed: _onSend,
+        backgroundColor: theme.colorScheme.secondary,
+        child: Icon(Icons.send),
+      );
+    else if (_tabIndex == contactTabIndex)
+      button = FloatingActionButton(
+        onPressed: _onAddContact,
+        backgroundColor: theme.colorScheme.secondary,
+        child: Icon(Icons.add),
+      );
+
+    final menu = PopupMenuButton<String>(
+      itemBuilder: (context) {
+        return [
+          PopupMenuItem(child: Text(s.accounts), value: "Accounts"),
+          PopupMenuItem(child: Text(s.backup), value: "Backup"),
+          PopupMenuItem(child: Text(s.rescan), value: "Rescan"),
+          if (!simpleMode && active.canPay)
+            PopupMenuItem(child: Text(s.coldStorage), value: "Cold"),
+          if (!simpleMode)
+            PopupMenuItem(child: Text(s.multipay), value: "MultiPay"),
+          if (!simpleMode)
+            PopupMenuItem(child: Text(s.broadcast), value: "Broadcast"),
+          PopupMenuItem(child: Text(s.settings), value: "Settings"),
+          PopupMenuItem(child: Text(s.help), value: "Help"),
+          PopupMenuItem(child: Text(s.about), value: "About"),
+        ];
+      },
+      onSelected: _onMenu,
+    );
+
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("${active.account.name}"),
+        bottom: TabBar(
+          controller: _tabController,
+          isScrollable: true,
+          tabs: [
+            Tab(text: s.account),
+            if (!simpleMode) Tab(text: s.notes),
+            Tab(text: s.history),
+            if (!simpleMode) Tab(text: s.budget),
+            if (!simpleMode) Tab(text: s.tradingPl),
+            Tab(text: s.contacts),
+          ],
+        ),
+        actions: [menu],
+      ),
+      body: TabBarView(
+        controller: _tabController,
+        children: [
+          AccountPage2(),
+          if (!simpleMode) NoteWidget(),
+          HistoryWidget(),
+          if (!simpleMode) BudgetWidget(),
+          if (!simpleMode) PnLWidget(),
+          ContactsTab(key: contactKey),
+        ],
+      ),
+      floatingActionButton: button,
+    );
+  }
 
   _onSend() {
     Navigator.of(this.context).pushNamed('/send');
