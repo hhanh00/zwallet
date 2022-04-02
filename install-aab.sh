ac0499a459eec4380dd48fa647ce5eb12952ca93
Hanh <hanh425@gmail.com>
2022-04-02 13:49:53 +0800

    Bump up build #

--- install-aab.sh
+++ install-aab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-adb uninstall me.hanh.zywallet
+adb uninstall me.hanh.ywallet
 bundletool build-apks --overwrite --bundle=build/app/outputs/bundle/release/app-release.aab --output=/tmp/app.apks --ks=docker/zwallet.jks --ks-key-alias=hanh --ks-pass=pass:$JKS_PASSWORD
 bundletool install-apks --adb=/usr/bin/adb --apks=/tmp/app.apks
