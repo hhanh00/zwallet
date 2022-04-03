847e8c066006b5165c8563b46319d8a183f5799e
Hanh <hanh425@gmail.com>
2022-04-03 14:20:49 +0800

    Skip 32 bit x86

--- build.sh
+++ build.sh
@@ -1,3 +1,2 @@
 cargo make --profile release
-flutter build apk --split-per-abi
 flutter build appbundle
--- install-deps.sh
+++ install-deps.sh
@@ -23,12 +23,9 @@ curl -o flutter.tar.xz https://storage.googleapis.com/flutter_infra_release/rele
 tar xvf flutter.tar.xz
 rm -f flutter.tar.xz
 
-curl -sSL https://git.io/get-mo -o /usr/bin/mo
-chmod +x /usr/bin/mo
-
-mkdir /root/.zcash-params
-curl https://download.z.cash/downloads/sapling-output.params -o /root/.zcash-params/sapling-output.params
-curl https://download.z.cash/downloads/sapling-spend.params -o /root/.zcash-params/sapling-spend.params
+mkdir $HOME/.zcash-params
+curl https://download.z.cash/downloads/sapling-output.params -o $HOME/.zcash-params/sapling-output.params
+curl https://download.z.cash/downloads/sapling-spend.params -o $HOME/.zcash-params/sapling-spend.params
 
 export ANDROID_NDK_HOME=$ANDROID_SDK_ROOT/ndk/21.4.7075529
 export PATH=$PATH:/flutter/bin
