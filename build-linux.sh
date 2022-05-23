375ebda8f975da12e0c5f5d859e83233fd6d514c
Hanh <hanh425@gmail.com>
2022-05-23 09:33:57 +0800

    Linux build update

--- build-linux.sh
+++ build-linux.sh
@@ -1,5 +1,6 @@
 # apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
 # (cd native/warp_api_ffi; cargo build --release)
+git config --global --add safe.directory /flutter
 flutter config --enable-linux-desktop
 flutter pub get
 flutter build linux
