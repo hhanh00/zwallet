d256155efc9a7296aa6525ed74816476bcdb06e7
Hanh <hanh425@gmail.com>
2022-07-15 09:26:55 +0800

    Update flutter

--- install-deps.sh
+++ install-deps.sh
@@ -14,7 +14,7 @@ pacman -Sy --noconfirm unzip jdk8-openjdk wget
 
 wget -P $DL_DIR -N https://dl.google.com/android/repository/commandlinetools-linux-7583922_latest.zip
 wget -P $DL_DIR -N https://dl.google.com/android/repository/android-ndk-r21e-linux-x86_64.zip
-wget -P $DL_DIR -N https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.0.4-stable.tar.xz
+wget -P $DL_DIR -N https://storage.googleapis.com/flutter_infra_release/releases/stable/linux/flutter_linux_3.0.5-stable.tar.xz
 
 wget -P $DL_DIR -N https://download.z.cash/downloads/sapling-output.params
 wget -P $DL_DIR -N https://download.z.cash/downloads/sapling-spend.params
@@ -28,7 +28,7 @@ yes | ./sdkmanager --sdk_root=$ANDROID_SDK_ROOT --licenses &&
 yes | ./sdkmanager --sdk_root=$ANDROID_SDK_ROOT "platform-tools" "platforms;android-31")
 
 (cd $ANDROID_SDK_ROOT;unzip -o $DL_DIR/android-ndk-r21e-linux-x86_64.zip)
-(cd $ROOT_DIR;tar xvf $DL_DIR/flutter_linux_3.0.4-stable.tar.xz)
+(cd $ROOT_DIR;tar xvf $DL_DIR/flutter_linux_3.0.5-stable.tar.xz)
 
 mkdir -p $HOME/.zcash-params
 cp $DL_DIR/sapling-output.params $DL_DIR/sapling-spend.params $HOME/.zcash-params
