5bd1b3cffe5f2df325a11510fc9f5c57f243e58f
Hanh <hanh425@gmail.com>
2022-09-24 20:05:45 +0800

    Catch exception when disconnected

--- lib/contact.dart
+++ lib/contact.dart
@@ -95,7 +95,7 @@ class ContactsState extends State<ContactsTab> {
         S.of(context).ok);
     if (approve) {
       final tx = await WarpApi.commitUnsavedContacts(settings.anchorOffset);
-      showSnackBar("${S.of(context).txId}: $tx");
+      showSnackBar(S.of(context).txId(tx));
       contacts.markContactsSaved(active.coin, true);
     }
   }
