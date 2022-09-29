905b93443a257b097629b141c994e97e9e3f584c
hhanh00 <hhanh00@users.noreply.github.com>
2022-09-29 11:00:11 +0000

    deploy: 81572f16ce1f165ff49a060b9021a57ce6010034

--- js/hugo-learn.js
+++ js/hugo-learn.js
@@ -29,37 +29,6 @@ images.wrap(function(){
   }
 });
 
-// Change styles, depending on parameters set to the image
-images.each(function(index){
-  var image = $(this)
-  var o = getUrlParameter(image[0].src);
-  if (typeof o !== "undefined") {
-    var h = o["height"];
-    var w = o["width"];
-    var c = o["classes"];
-    image.css("width", function() {
-      if (typeof w !== "undefined") {
-        return w;
-      } else {
-        return "auto";
-      }
-    });
-    image.css("height", function() {
-      if (typeof h !== "undefined") {
-        return h;
-      } else {
-        return "auto";
-      }
-    });
-    if (typeof c !== "undefined") {
-      var classes = c.split(',');
-      for (i = 0; i < classes.length; i++) {
-        image.addClass(classes[i]);
-      }
-    }
-  }
-});
-
 // Stick the top to the top of the screen when  scrolling
 $(document).ready(function(){
   $("#top-bar").sticky({topSpacing:0, zIndex: 1000});
