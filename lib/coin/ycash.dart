a68016d9f9d873de82e1ed78573d394013257320
Hanh <hanh425@gmail.com>
2022-11-20 10:56:11 +0800

    Add ZcashTest coin

--- lib/coin/ycash.dart
+++ lib/coin/ycash.dart
@@ -4,11 +4,12 @@ import "coin.dart";
 
 class YcashCoin extends CoinBase {
   int coin = 1;
+  String name = "Ycash";
   String app = "YWallet";
   String symbol = "\u24E8";
   String currency = "ycash";
-  String ticker = "YEC";
   int coinIndex = 347;
+  String ticker = "YEC";
   String dbName = "yec.db";
   String explorerUrl = "https://yecblockexplorer.com/tx/";
   AssetImage image = AssetImage('assets/ycash.png');
--- lib/coin/zcashtest.dart
+++ lib/coin/zcashtest.dart
@@ -1,15 +1,22 @@
+import 'package:flutter/material.dart';
+
 import 'coin.dart';
 
-class Coin {
-  String app = "ZWalletTest";
+class ZcashTestCoin extends CoinBase {
+  int coin = 0;
+  String name = "Zcash Test";
+  String app = "ZWallet";
   String symbol = "\u24E9";
   String currency = "zcash";
+  int coinIndex = 133;
   String ticker = "ZEC";
-  String explorerUrl = "https://sochain.com/tx/ZECTEST/";
+  String dbName = "zec-test.db";
+  String explorerUrl = "https://explorer.zcha.in/transactions/";
+  AssetImage image = AssetImage('assets/zcash.png');
   List<LWInstance> lwd = [
     LWInstance("Lightwalletd", "https://testnet.lightwalletd.com:9067"),
   ];
-  bool supportsUA = false;
-  bool supportsMultisig = true;
+  bool supportsUA = true;
+  bool supportsMultisig = false;
   List<double> weights = [0.05, 0.25, 2.50];
 }
