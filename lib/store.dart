c2f068701372b70f37590cc53ac0b68dc08c1c50
Hanh <hanh425@gmail.com>
2022-11-24 17:33:03 +0800

    Fix colors on splash screen in light mode

--- lib/store.dart
+++ lib/store.dart
@@ -8,7 +8,6 @@ import 'package:json_annotation/json_annotation.dart';
 import 'package:flutter/material.dart';
 import 'package:mobx/mobx.dart';
 import 'package:package_info_plus/package_info_plus.dart';
-import 'package:path_provider/path_provider.dart';
 import 'coin/coins.dart';
 import 'package:warp_api/warp_api.dart';
 import 'package:warp_api/types.dart';
