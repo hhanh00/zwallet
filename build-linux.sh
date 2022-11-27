bfb215577fe46c24588b27725692144a4e5ca58a
Hanh <hanh425@gmail.com>
2022-11-27 12:00:22 +0800

    Flatpak github action

--- build-linux.sh
+++ build-linux.sh
@@ -1,6 +1,8 @@
 # apt-get install clang cmake ninja-build pkg-config libgtk-3-dev liblzma-dev
-# (cd native/warp_api_ffi; cargo build --release)
 git config --global --add safe.directory /flutter
 flutter config --enable-linux-desktop
 flutter pub get
+flutter pub run build_runner build
+cargo make flatpak
+(cd packages/warp_api_ffi;flutter pub get;flutter pub run build_runner build)
 flutter build linux
