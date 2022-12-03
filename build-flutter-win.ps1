ecee71568f307ec90e6cd8db3ba1c46ce8cdb85b
Hanh <hanh425@gmail.com>
2022-12-03 15:07:33 +0800

    github action for windows build

---
+++ build-flutter-win.ps1
@@ -0,0 +1,20 @@
+Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile \windows\system32\nuget.exe
+nuget sources add -Source https://api.nuget.org/v3/index.json -Name nuget.org
+
+flutter pub get
+flutter pub run build_runner build
+pushd packages\warp_api_ffi
+flutter pub get
+flutter pub run build_runner build
+popd
+
+flutter build windows
+pushd build\windows\runner
+rename-item Release ywallet
+popd
+copy runtime\* build\windows\runner\ywallet
+copy warp_api_ffi.dll build\windows\runner\ywallet
+
+pushd build\windows\runner
+Compress-Archive -Path ywallet\ -DestinationPath ..\..\..\ywallet.zip
+popd
