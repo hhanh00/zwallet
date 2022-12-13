fd9d19f69c0e68df63315ac1fb8cb21b7c5af019
Hanh <hanh425@gmail.com>
2022-12-14 04:09:45 +0800

    flathub bins

--- docker/flatpak-build.sh
+++ docker/flatpak-build.sh
@@ -5,3 +5,5 @@ docker rm zwallet_flatpak_bin
 docker build -f docker/Dockerfile-flatpak -t zwallet_flatpak_sh .
 docker run --privileged --name zwallet_flatpak_bin zwallet_flatpak_sh
 docker cp zwallet_flatpak_bin:/root/misc/ywallet.flatpak .
+docker cp zwallet_flatpak_bin:/root/misc/root/zwallet.tgz .
+docker cp zwallet_flatpak_bin:/root/misc/root/libwarp_api_ffi.so .
