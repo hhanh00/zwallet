8153f55802494bbbdd0ed8a851aae87acee88d8e
Hanh <hanh425@gmail.com>
2023-01-21 19:08:28 +0800

    fdroid metadata

--- install-aab.sh
+++ install-aab.sh
@@ -2,4 +2,4 @@
 
 adb uninstall me.hanh.ywallet
 bundletool build-apks --overwrite --bundle=build/app/outputs/bundle/release/app-release.aab --output=/tmp/app.apks --ks=docker/zwallet.jks --ks-key-alias=hanh --ks-pass=pass:$JKS_PASSWORD
-bundletool install-apks --adb=/usr/bin/adb --apks=/tmp/app.apks
+bundletool install-apks --adb=$ANDROID_SDK_ROOT/platform-tools/adb --apks=/tmp/app.apks
--- install-rust.sh
+++ install-rust.sh
@@ -2,5 +2,5 @@
 
 sudo pacman -Sy --noconfirm git cmake rustup
 rustup install stable
-rustup target add aarch64-linux-android armv7-linux-androideabi x86_64-linux-android
-cargo install --force cargo-make cargo-ndk
+rustup target add aarch64-linux-android armv7-linux-androideabi
+cargo install --force cargo-ndk
