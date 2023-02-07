12b0853ada96dc3e28e386b4ca2b3dfa29eebf79
Hanh <hanh425@gmail.com>
2023-02-07 17:57:43 +1000

    Publish to GP workflow

--- install-aab.sh
+++ install-aab.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 adb uninstall me.hanh.ywallet
-bundletool build-apks --overwrite --bundle=build/app/outputs/bundle/release/app-release.aab --output=/tmp/app.apks --ks=docker/zwallet.jks --ks-key-alias=hanh --ks-pass=pass:$JKS_PASSWORD
+bundletool build-apks --overwrite --bundle=build/app/outputs/bundle/release/app-release.aab --output=/tmp/app.apks --ks=docker/zwallet.jks --ks-key-alias=zwallet --ks-pass=pass:$JKS_PASSWORD
 bundletool install-apks --adb=$ANDROID_SDK_ROOT/platform-tools/adb --apks=/tmp/app.apks
