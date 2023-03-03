d26a0458633009c8bf0c1e315510653c70fd6133
Hanh <hanh425@gmail.com>
2023-03-03 21:11:45 +1000

    Merge flatpak and appimage builders

---
+++ build-scripts/build-appimage.sh
@@ -0,0 +1,8 @@
+sudo apt-get install -y binutils coreutils desktop-file-utils fakeroot fuse libgdk-pixbuf2.0-dev patchelf python3-pip python3-setuptools squashfs-tools strace util-linux zsync
+sudo wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage -O /usr/local/bin/appimagetool
+sudo chmod +x /usr/local/bin/appimagetool
+sudo pip3 install appimage-builder
+
+pushd misc
+appimage-builder --recipe AppImageBuilder.yml
+popd
--- build-scripts/build-flatpak2.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-flatpak-builder --user --install --force-clean build-dir me.hanh.zwallet.Ywallet.yml
-flatpak build-export /root/repo build-dir
-flatpak build-bundle /root/repo ywallet.flatpak me.hanh.zwallet.Ywallet
