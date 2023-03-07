3c830356234d7a9a8d92712a86c2881ff6b57104
Hanh <hanh425@gmail.com>
2023-03-07 16:09:06 +1000

    Bundle OpenSSL & SQLCipher

---
+++ build-scripts/build-win2.sh
@@ -0,0 +1,21 @@
+# Codegen
+flutter pub get
+flutter pub run build_runner build -d
+pushd packages/warp_api_ffi
+flutter pub get
+flutter pub run build_runner build -d
+popd
+
+# Build flutter
+flutter build windows
+cp runtime/* build/windows/runner/release
+cp target/release/warp_api_ffi.dll build/windows/runner/release
+flutter pub run msix:create
+mv build/windows/runner/Release/YWallet.msix .
+
+flutter build windows
+cp runtime/* build/windows/runner/Release
+pushd build/windows/runner
+mv Release ywallet
+7z a ../../../ywallet.zip ywallet
+popd
