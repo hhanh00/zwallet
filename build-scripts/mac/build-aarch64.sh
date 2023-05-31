e2f83f6c077c394a7c693b114b775b31fe733aae
Hanh <hanh425@gmail.com>
2023-05-31 23:50:44 +1000

    Scan transaction

--- build-scripts/mac/build-aarch64.sh
+++ build-scripts/mac/build-aarch64.sh
@@ -2,4 +2,4 @@ set -x
 rustup target add aarch64-apple-darwin
 cargo b -r --target=aarch64-apple-darwin --features=dart_ffi,sqlcipher,ledger
 cd target/aarch64-apple-darwin/release
-rm -rf build examples deps incremental
+rm -rf build examples deps incremental .fingerprint *.rlib
