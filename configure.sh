26c1f42ef489de9db7e9dfec6eed04ef728b621d
Hanh <hanh425@gmail.com>
2023-10-21 22:34:45 +0800

    wip

--- configure.sh
+++ configure.sh
@@ -4,8 +4,8 @@ flutter pub run flutter_launcher_icons:main
 #flutter pub run flutter_app_name
 #flutter pub run change_app_package_name:main me.hanh.ywallet
 flutter gen-l10n
-flutter pub run build_runner build
+flutter pub run build_runner build -d
 #flutter pub run flutter_native_splash:create
 
-(cd packages/warp_api_ffi; flutter pub get; flutter pub run build_runner build)
+(cd packages/warp_api_ffi; flutter pub get; flutter pub run build_runner build -d)
 
