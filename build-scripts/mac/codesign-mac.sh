ae6defe69e3a44fb93204dc380a5e0ec811d8bda
Hanh <hanh425@gmail.com>
2023-10-28 12:57:03 +1000

    codesign

--- build-scripts/mac/codesign-mac.sh
+++ build-scripts/mac/codesign-mac.sh
@@ -1,8 +1,9 @@
 # cert.p12 cert_pwd keychain_pwd cert_id
+set -x
 echo $1 | base64 --decode > certificate.p12
-security create-keychain -p $3 build.keychain
+security create-keychain -p "$3" build.keychain
 security default-keychain -s build.keychain
-security unlock-keychain -p $3 build.keychain
-security import certificate.p12 -k build.keychain -P $2 -T /usr/bin/codesign
-security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k $3 build.keychain
-/usr/bin/codesign --force -s $4 --deep --options runtime build/macos/Build/Products/Release/ywallet.app -v
+security unlock-keychain -p "$3" build.keychain
+security import certificate.p12 -k build.keychain -P "$2" -T /usr/bin/codesign
+security set-key-partition-list -S apple-tool:,apple:,codesign: -s -k "$3" build.keychain
+/usr/bin/codesign --force -s "$4" --deep --options runtime build/macos/Build/Products/Release/ywallet.app -v
