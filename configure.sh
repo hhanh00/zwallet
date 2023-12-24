8d1c96992ef9790ac7bc3bbabe7b0f676578f3b3
Hanh <hanh425@gmail.com>
2023-12-24 19:29:22 +1000

    Launch Images iOS

--- configure.sh
+++ configure.sh
@@ -1,6 +1,7 @@
 dart pub get
 dart pub upgrade
 dart run flutter_launcher_icons:main
+dart run flutter_native_splash:create
 #flutter pub run flutter_app_name
 #flutter pub run change_app_package_name:main me.hanh.ywallet
 flutter gen-l10n
